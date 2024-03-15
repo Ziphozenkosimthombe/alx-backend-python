bf5a458904e9f47d9a83e12d1e20ce848989929c
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-15 13:27:24 +0000

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-- Given the parameters and the return values, add type annotations to the function
+- Given the parameters and the return values,
+ add type annotations to the function
 - Hint: look into TypeVar
 def safely_get_value(dct, key, default = None):
     if key in dct:
@@ -12,8 +13,6 @@ def safely_get_value(dct, key, default = None):
 
 
 import typing
-
-
 
 T = typing.TypeVar('T')
 
