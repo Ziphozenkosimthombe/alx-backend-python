992d0f9c8e1ca92fb2696d955d699520f6242a33
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-15 12:08:25 +0000

    8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,6 @@
 from typing import Callable
 
 
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiply(n: float) -> float:
         """ Return the product of a float by a multiplier"""
