07966358719709577ba022c10a2dba19d19ff878
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-15 15:09:50 +0200

    100-safe_first_element.py

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import Sequence, Union, Any, NonType
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NonType]:
+    """ Return the first element of a list """
+    if lst:
+        return lst[0]
+    else:
+        return None
