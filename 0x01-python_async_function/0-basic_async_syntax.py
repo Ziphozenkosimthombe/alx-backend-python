d7fe6cadc7d7d16b5ce62792cf68b446604b7fc7
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-18 10:40:58 +0000

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,9 @@
 
 """
 - Write an asynchronous coroutine that takes in an integer argument (max_delay
-- with a default value of 10) named wait_random that waits for a random delay between 0 and max_delay (included and float value) seconds and eventually returns it.
+- with a default value of 10) named wait_random that waits,
+- for a random delay between 0 and max_delay (included and float value),
+- seconds and eventually returns it.
 - Use the random module
 """
 
@@ -12,6 +14,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """"returns a random float value"""
-    random_delay:float = random.uniform(0, max_delay)
+    random_delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
